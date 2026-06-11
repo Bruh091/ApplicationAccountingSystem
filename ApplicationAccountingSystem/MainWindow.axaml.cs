d1a8b8c743a61daed78580471dd098f1286255bc
Egitva <9026479948e@gmail.com>
2026-06-11 19:46:36 +0500

    Добавьте файлы проекта.

---
+++ ApplicationAccountingSystem/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace ApplicationAccountingSystem
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ApplicationAccountingSystem/Model/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationAccountingSystem.Model
+{
+    public class User
+    {
+        public Guid id { get; set; }
+        public string Username { get; set; }
+        public string PasswordHash { get; set; }
+        public string Email { get; set; }
+
+    }
+}
