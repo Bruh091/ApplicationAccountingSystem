22f3d5bb6e9427ba3b6416145138214750577c46
vadim36 <vadim1608@gmail.com>
2026-06-25 13:36:03 +0500

    Сделал модель TicketHistory чтобы сделедить за историей изменения тикета

---
+++ ApplicationAccountingSystem/Model/TicketHistory.cs
@@ -0,0 +1,14 @@
+using System;
+namespace ApplicationAccountingSystem.Model.TicketHistory
+{
+    public class TicketHistory
+    {
+        public Guid Id { get; set; }
+        public string Field { get; set; }
+        public string OldValue { get; set; }
+        public string NewValue { get; set; }
+        public DateTime ChangedAt { get; set; }
+        public Guid ChangedById { get; set; }
+        public Guid TicketId { get; set; }
+    }    
+}
