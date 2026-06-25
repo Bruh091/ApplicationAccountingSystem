7e82167ea53d7f50b071ef16d3f89dcd073acb00
vadim36 <vadim1608@gmail.com>
2026-06-25 13:25:46 +0500

    Сделал модель Ticket и добавил обозначение Статуса тикета и его приоритета TicketStatus, TicketPriotiry

--- ApplicationAccountingSystem/Model/User.cs
+++ ApplicationAccountingSystem/Model/User.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using ApplicationAccountingSystem.Role.UserRole;
+using ApplicationAccountingSystem.Designation.UserRole;
 namespace ApplicationAccountingSystem.Model
 {
     public class User
