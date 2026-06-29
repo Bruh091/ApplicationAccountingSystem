4a81ee975d0b34e8ba8b7bff896cf1e1a15cea1b
vadim36 <vadim1608@gmail.com>
2026-06-29 11:12:17 +0500

    Добавил GetPreview в Comment для краткого просмотра

--- ApplicationAccountingSystem/Domain/Model/Comment.cs
+++ ApplicationAccountingSystem/Domain/Model/Comment.cs
@@ -9,5 +9,13 @@ namespace ApplicationAccountingSystem.Domain.Model
         public bool IsInternal { get; set; } = false;
         public Guid UserId { get; set; }
         public Guid TicketId { get; set; }
+        public string GetPreview()
+        {
+            if (Content.Length <= 67)
+            {
+                return Content;
+            }
+            return Content.Substring(0, 50) + "...";
+        }
     }
 }
