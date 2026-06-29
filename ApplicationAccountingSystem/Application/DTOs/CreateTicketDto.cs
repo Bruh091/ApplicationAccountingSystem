38ec42f9f4b15f747dc291bbd6383f21a1f855c9
vadim36 <vadim1608@gmail.com>
2026-06-29 11:54:36 +0500

    Создал createTicketDTO

---
+++ ApplicationAccountingSystem/Application/DTOs/CreateTicketDto.cs
@@ -0,0 +1,13 @@
+using System;
+using ApplicationAccountingSystem.Domain.Designation;
+
+namespace ApplicationAccountingSystem.Application.DTOs
+{
+    public class CreateTicketDto
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public TicketPriority Priority { get; set; }
+        public Guid CreatedById { get; set; }
+    }
+}
