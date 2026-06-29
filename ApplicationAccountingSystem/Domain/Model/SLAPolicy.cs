44254ae852d3c29272d20c26169c6b781332f951
vadim36 <vadim1608@gmail.com>
2026-06-29 11:23:48 +0500

    Добавил пару методов в SLAPolicy

--- ApplicationAccountingSystem/Domain/Model/SLAPolicy.cs
+++ ApplicationAccountingSystem/Domain/Model/SLAPolicy.cs
@@ -9,5 +9,15 @@ namespace ApplicationAccountingSystem.Domain.Model
         public int ResponseTimeInHours { get; set; }
         public int ResolutionTimeInHours { get; set; }
         public bool IsActive { get; set; } = true;
+        public DateTime CalculateDeadline(DateTime createAt)
+        {
+            return createAt.AddHours(ResolutionTimeInHours);
+        }
+        public bool CheckViolatoin(DateTime createAt, DateTime? resolvedAt = null)
+        {
+            var deadline = CalculateDeadline(createAt);
+            var compareDate = resolvedAt ?? DateTime.UtcNow;
+            return compareDate > deadline;
+        }
     }
 }
