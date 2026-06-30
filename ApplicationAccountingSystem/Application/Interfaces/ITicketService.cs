8e5b3f7dcac9e24595054e5482014a039225f7ee
Haqz.dev <105918255+Hqzdev@users.noreply.github.com>
2026-06-30 19:20:41 +0500

    Дополнил ITicketService

--- ApplicationAccountingSystem/Application/Interfaces/ITicketService.cs
+++ ApplicationAccountingSystem/Application/Interfaces/ITicketService.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using ApplicationAccountingSystem.Domain.Designation;
 using ApplicationAccountingSystem.Application.DTOs;
 
 namespace ApplicationAccountingSystem.Application.Interface
@@ -11,5 +12,8 @@ namespace ApplicationAccountingSystem.Application.Interface
         Task<TicketDto?> GetTicketByIdAsync(Guid ticketId);
         Task<IEnumerable<TicketDto>> GetAllTicketsAsync();
         Task<IEnumerable<TicketDto>> GetTicketsByUserIdAsync(Guid userId);
+        Task<TicketDto?> AssignTicketAsync(Guid ticketId, Guid agentId);
+        Task<TicketDto?> ChangeStatusAsync(Guid ticketId, TicketStatus status);
+        Task<TicketDto?> ChangePriorityAsync(Guid ticketId, TicketPriority priority);
     }
 }
