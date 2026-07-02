286380a65601e13d87f7cb29f19771796f0c4adf
Arikama-sudo <mariagrayfer@mail.ru>
2026-07-02 19:15:34 +0500

    Реализовал UserRepository в Infrastucture/Repository

---
+++ ApplicationAccountingSystem/Infrastructure/Repository/UserRepository.cs
@@ -0,0 +1,64 @@
+﻿using ApplicationAccountingSystem.Domain.Designation;
+using ApplicationAccountingSystem.Domain.Interfaces;
+using ApplicationAccountingSystem.Domain.Model;
+using ApplicationAccountingSystem.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationAccountingSystem.Infrastructure.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly AppDbContext _context;
+        public UserRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<User?> CreateUserAsync(User user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+            return user;
+        }
+        public async Task<User?> GetUserByIdAsync(Guid userId)
+        {
+            return await _context.Users.FindAsync(userId);
+        }
+        public async Task<User?> GetUserByUsernameAsync(string username)
+        {
+            return await _context.Users.FirstOrDefaultAsync(x => x.Username == username);
+        }
+        public async Task<User?> GetUserByEmailAsync(string email)
+        {
+            return await _context.Users.FirstOrDefaultAsync(x => x.Email == email);
+        }
+        public async Task<IEnumerable<User>> GetAllUsersAsync()
+        {
+            return await _context.Users.ToListAsync();
+        }
+        public async Task<IEnumerable<User>> GetUsersByRoleAsync(UserRole userRole)
+        {
+            return await _context.Users.Where(x => x.Role == userRole).ToListAsync();
+        }
+        public async Task UpdateUserAsync(User user)
+        {
+            _context.Users.Update(user);
+            await _context.SaveChangesAsync();
+        }
+        public async Task DeactivateUserAsync(Guid userId)
+        {
+            var user = await GetUserByIdAsync(userId);
+            if (user == null)
+            {
+                return;
+            }
+            user.IsActive = false;
+            await UpdateUserAsync(user);
+        }
+    }
+}
