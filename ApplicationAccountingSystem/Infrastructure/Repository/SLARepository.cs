2bbdd32a7daaf9c73aa6cb34fe0e88e9bf197e5c
Arikama-sudo <mariagrayfer@mail.ru>
2026-07-02 19:56:52 +0500

    Частично реализовал SLARepository

---
+++ ApplicationAccountingSystem/Infrastructure/Repository/SLARepository.cs
@@ -0,0 +1,39 @@
+﻿using ApplicationAccountingSystem.Domain.Designation;
+using ApplicationAccountingSystem.Domain.Interfaces;
+using ApplicationAccountingSystem.Domain.Model;
+using ApplicationAccountingSystem.Infrastructure.Data;
+using Avalonia.Media;
+using Avalonia.Platform;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationAccountingSystem.Infrastructure.Repository
+{
+    public class SLARepository : ISLARepository
+    {
+        private readonly AppDbContext _context;
+        public SLARepository(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<SLAPolicy> CreateSLAAsync(SLAPolicy sla)
+        {
+            _context.SLAPolicies.Add(sla);
+            await _context.SaveChangesAsync();
+            return sla;
+        }
+        public async Task<SLAPolicy?> GetSLAByIdAsync(Guid slaId)
+        {
+            return await _context.SLAPolicies.FindAsync(slaId);
+        }
+        public async Task<SLAPolicy?> GetSLAPolicyByPriorityAsync(TicketPriority priority)
+        {
+            return await _context.SLAPolicies.Where(x=>x.Priority == priority).FirstOrDefaultAsync();
+        }
+    }
+}
